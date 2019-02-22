4fc6212ce24c4ec8eb052eaf01ac59e18cb1793a
David Lopez <david.lopez@ryerson.ca>
2019-02-22 12:44:29 -0500

    first

---
+++ iroha/Setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+from iroha import Iroha, IrohaGrpc
+from iroha import IrohaCrypto
+import binascii
+import sys
+if sys.version_info[0] < 3:
+    raise Exception('Python 3 or a more recent version is required.')
+
+
+admin_private_key = 'f101537e319568c765b2cc89698325604991dca57b9716b58016b253506cab70'
+iroha = Iroha('admin@test')
+net = IrohaGrpc()
+
+
+def trace(func):
+    """
+    A decorator for tracing methods' begin/end execution points
+    """
+
+    def tracer(*args, **kwargs):
+        name = func.__name__
+        print('\tEntering "{}"'.format(name))
+        result = func(*args, **kwargs)
+        print('\tLeaving "{}"'.format(name))
+        return result
+
+    return tracer
+
+
+@trace
+def send_transaction_and_print_status(transaction):
+    hex_hash = binascii.hexlify(IrohaCrypto.hash(transaction))
+    print('Transaction hash = {}, creator = {}'.format(
+        hex_hash, transaction.payload.reduced_payload.creator_account_id))
+    net.send_tx(transaction)
+    for status in net.tx_status_stream(transaction):
+        print(status)
+
+
+
+# Create a domain and an asset. The assets are the parameter we use for machine learning
+@trace
+def create_domain_and_asset():
+    """
+    Creates domain 'domain' and asset 'parameter#domain' with precision 2
+    """
+    commands = [
+        iroha.command('CreateDomain', domain_id='federating', default_role='user'),
+        iroha.command('CreateAsset', asset_name='fedcoin', domain_id='federating', precision=2)
+    ]
+    tx = IrohaCrypto.sign_transaction(
+        iroha.transaction(commands), admin_private_key)
+    send_transaction_and_print_status(tx)
+
+create_domain_and_asset()
+
+print('done')
---
+++ iroha/node.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from iroha import Iroha, IrohaGrpc
+from iroha import IrohaCrypto
+import sys
+import functions
+if sys.version_info[0] < 3:
+    raise Exception('Python 3 or a more recent version is required.')
+
+
+# user_private_key = IrohaCrypto.private_key()
+user_private_key = 'b11224fedce8e8deebf4c03339f16790681f35ca09ccdc9785a7217394065af5'
+user_public_key = IrohaCrypto.derive_public_key(user_private_key)
+name = 'generator'
+domain = 'federating'
+account_id = name + '@' + domain
+iroha = Iroha('generator@federating')
+network = IrohaGrpc()
+
+
+functions.create_account_user(name,user_public_key,domain,'1000')
+
+data = functions.get_balance(iroha,network,account_id,user_private_key)
+
+
+functions.grants_access_to_set_details()
+functions.set_detail_to_node()
+print('Resultados')
+print(data)
+
+
